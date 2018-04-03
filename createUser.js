46357a903857c321770982f90e3bf3d6ae3d0d8e
heatingdevice <ipadlover8322@gmail.com>
2018-04-03 07:01:23 -0400

    Create user script so yeah

--- createUser.js
+++ createUser.js
@@ -1,8 +1,18 @@
 const { User } = require("./schemas.js");
+const mongoose = require("mongoose");
+const utils = require("./utilities.js");
+mongoose.connect("localhost", "dickrc");
 
-new User({
-  id: 69,
-  nick: "beepy"
-})
-  .save()
-  .then(r => console.log(r));
+(async () => {
+  try {
+    await new User({
+      id: await utils.generateSnowflake(),
+      nick: process.argv[2],
+      hash: await utils.createHash(),
+      password: process.argv[3]
+    }).save();
+  } catch (err) {
+    console.log("lmaole", err);
+  }
+  mongoose.disconnect();
+})();
